f7d6cdc9e0a7284a79511309b0774bca1cb6d2b9
peper11111 <peper.pleban@gmail.com>
2018-03-17 21:32:38 +0100

    Deleted custom styles. Added material-components-web, material icons and axios.

--- src/components/LoginView.vue
+++ src/components/LoginView.vue
@@ -1,12 +1,5 @@
 <template>
-<div class="login-view">
-  <h1>Cookbook</h1>
-  <form class="form">
-    <input class="form__input" type="text" :placeholder="$t('login')"/>
-    <input class="form__input" type="password" :placeholder="$t('password')"/>
-    <input class="form__input" type="submit" :value="$t('log-in')"/>
-  </form>
-</div>
+<div>Login View</div>
 </template>
 
 <script>
@@ -14,10 +7,3 @@ export default {
   name: 'LoginView'
 }
 </script>
-
-<style lang="scss">
-.login-view {
-  background-image: url("../assets/images/dan-gold-105699-unsplash.jpg");
-  font-family: "Dancing Script", cursive;
-}
-</style>
