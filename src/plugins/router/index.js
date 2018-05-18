919eeff802687cb3d2e9b36661cfbed6da2c9b3f
peper11111 <peper.pleban@gmail.com>
2018-05-19 00:30:41 +0200

    Moved routes to main router init file.

--- src/plugins/router/index.js
+++ src/plugins/router/index.js
@@ -3,24 +3,35 @@ import VueRouter from 'vue-router'
 import api from '@/plugins/api'
 import store from '@/plugins/store'
 import { SET_AUTH } from '@/plugins/store/mutation-types'
-import routes from './routes'
 
 Vue.use(VueRouter)
+
+const ConfirmView = () => import('@/pages/ConfirmView')
+const FavoriteView = () => import('@/pages/FavoriteView')
+const HomeView = () => import('@/pages/HomeView')
+const LoginView = () => import('@/pages/LoginView')
+const NewRecipeView = () => import('@/pages/NewRecipeView')
+const ProfileView = () => import('@/pages/ProfileView')
+const RegisterView = () => import('@/pages/RegisterView')
+const ResetView = () => import('@/pages/ResetView')
+const VerifyView = () => import('@/pages/VerifyView')
 
 const router = new VueRouter({
   mode: 'history',
   linkActiveClass: 'active',
-  routes
+  routes: [
+    { path: '/', component: HomeView, meta: { requiresAuth: true } },
+    { path: '/favorite', component: FavoriteView, meta: { requiresAuth: true } },
+    { path: '/profile', component: ProfileView, meta: { requiresAuth: true } },
+    { path: '/new-recipe', component: NewRecipeView, meta: { requiresAuth: true } },
+    { path: '/login', component: LoginView },
+    { path: '/register', component: RegisterView },
+    { path: '/reset', component: ResetView },
+    { path: '/confirm', component: ConfirmView },
+    { path: '/verify', component: VerifyView },
+    { path: '*', redirect: '/' }
+  ]
 })
-
-router.beforeEach(async (to, from, next) => {
-  if (store.state.auth.loggedIn === null) {
-    await api.auth.current().then(value => {
-      store.commit(SET_AUTH, { loggedIn: true, currentUserId: value.data })
-    }).catch(() => {
-      store.commit(SET_AUTH, { loggedIn: false, currentUserId: null })
-    })
-  }
 
   if (to.matched.some(record => record.meta.requiresAuth)) {
     if (!store.state.auth.loggedIn) {
