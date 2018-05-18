4149f7148bc090fa69acce4b282fced5e00c2f97
peper11111 <peper.pleban@gmail.com>
2018-05-18 23:18:34 +0200

    Wrapped axios library as Vue plugin accessible in component at $http.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import api from './plugins/api'
-import i18n from './plugins/i18n'
-import router from './plugins/router'
-import store from './plugins/store'
+import api from '@/plugins/api'
+import http from '@/plugins/http'
+import i18n from '@/plugins/i18n'
+import router from '@/plugins/router'
+import store from '@/plugins/store'
 
 import './assets/styles/main.scss'
 
@@ -14,6 +15,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   api,
+  http,
   i18n,
   router,
   store,
---
+++ src/plugins/http/index.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+import Vue from 'vue'
+
+import router from '@/plugins/router'
+import store from '@/plugins/store'
+import { SET_AUTH } from '@/plugins/store/mutation-types'
+
+axios.install = (_Vue) => {
+  _Vue.mixin({
+    beforeCreate () {
+      const options = this.$options
+      if (options.http) {
+        this.$http = options.http
+      } else if (options.parent && options.parent.$http) {
+        this.$http = options.parent.$http
+      }
+    }
+  })
+}
+
+Vue.use(axios)
+
+const http = axios.create({
+  baseURL: 'http://localhost:8000/api/v1',
+  withCredentials: true
+})
+
+http.interceptors.response.use(value => value, error => {
+  if (error.response.status === 401) {
+    store.commit(SET_AUTH, { loggedIn: false, currentUserId: null })
+    router.push('/login')
+  }
+  return Promise.reject(error)
+})
+
+export default http
