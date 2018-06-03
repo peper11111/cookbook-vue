38ee39733793472d1674944d3e6f1f5330a95ec6
peper11111 <peper.pleban@gmail.com>
2018-06-03 23:12:25 +0200

    Replaced old div with proper component.

--- src/pages/new-recipe-page.vue
+++ src/pages/new-recipe-page.vue
@@ -1,12 +1,11 @@
 <template>
-<div class="view">
-  <div class="view__wrapper recipe">
-    <div class="recipe__image">
-      <img src="https://d1doqjmisr497k.cloudfront.net/-/media/kamispl-2016/recipe/2000/paella_valencia_2000.ashx?vd=20160620T210802Z&ir=1&width=885&height=498&crop=auto&quality=75&speed=0&hash=567B2A8FB6FB9571966589138FC1807A33821135"/>
-      <div class="recipe__overlay">
-        <i class="material-icons">camera_alt</i>
-      </div>
-    </div>
+<div class="o-page">
+  <div class="o-page__wrapper">
+    <image-picker
+      :editMode="true"
+      :imgSrc="'https://d1doqjmisr497k.cloudfront.net/-/media/kamispl-2016/recipe/2000/paella_valencia_2000.ashx?vd=20160620T210802Z&ir=1&width=885&height=498&crop=auto&quality=75&speed=0&hash=567B2A8FB6FB9571966589138FC1807A33821135'"
+      class="recipe__banner"
+    ></image-picker>
     <div class="recipe__details">
       <div>
         <label class="recipe__label" v-text="$t('recipe.cuisine-type')"></label>
@@ -32,11 +31,11 @@
 </template>
 
 <script>
-import base from '@/mixins/base'
-
 export default {
   name: 'NewRecipeView',
-  mixins: [ base ],
+  components: {
+    ImagePicker: () => import('@/components/image-picker')
+  },
   computed: {
     cuisines () {
       return this.$store.state.recipe.cuisines
@@ -62,43 +61,9 @@ export default {
 @import '../assets/styles/variables';
 
 .recipe {
-  box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
-
-  &__image {
-    height: 400px;
-    position: relative;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-      object-position: center;
-    }
-  }
-
-  &__overlay {
-    position: absolute;
-    top: 0;
-    left: 0;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
+  &__banner {
     width: 100%;
-    height: 100%;
-    background-color: rgba(0, 0, 0, 0.25);
-    cursor: pointer;
-    opacity: 0;
-    user-select: none;
-
-    &:hover {
-      opacity: 1;
-    }
-
-    i {
-      font-size: 32px;
-      color: $color-white;
-    }
+    height: 300px;
   }
 
   &__details {
