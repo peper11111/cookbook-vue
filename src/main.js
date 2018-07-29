9a0e03ef529854e8534adb0abb3113e8f9252c75
Piotr Pleban <peper.pleban@gmail.com>
2018-07-30 01:05:47 +0200

    Set moment locale.

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import '@/assets/styles/main.scss'
+import moment from 'moment'
 import Vue from 'vue'
 import api from '@/plugins/api'
 import i18n from '@/plugins/i18n'
 import router from '@/plugins/router'
 import store from '@/plugins/store'
-import '@/assets/styles/main.scss'
+
+moment.locale('pl')
 
 Vue.config.productionTip = false
 
@@ -17,7 +20,7 @@ new Vue({
   router,
   store,
   components: {
-    App: () => import('./app')
+    App: () => import('@/app')
   },
   template: '<App/>'
 })
