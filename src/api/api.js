8c25981f3189c67961abefadc0d353e992df687b
Piotr Pleban <peper.pleban@gmail.com>
2018-09-30 11:43:39 +0200

    Changed Api const to class.

---
+++ src/api/api.js
@@ -0,0 +1,23 @@
+export default class Api {
+  constructor (options) {
+    for (const module in options.modules) {
+      if (!options.modules.hasOwnProperty(module)) {
+        continue
+      }
+      this[module] = options.modules[module]
+    }
+  }
+
+  static install (Vue) {
+    Vue.mixin({
+      beforeCreate () {
+        const options = this.$options
+        if (options.api) {
+          this.$api = options.api
+        } else if (options.parent && options.parent.$api) {
+          this.$api = options.parent.$api
+        }
+      }
+    })
+  }
+}
---
+++ src/api/modules.js
@@ -0,0 +1,52 @@
+import http from '@/api/http'
+
+export default {
+  auth: {
+    login: (data) => http.post('/auth/login', data),
+    logout: () => http.post('/auth/logout'),
+    register: (data) => http.post('/auth/register', data),
+    registerResend: (data) => http.post('/auth/register/resend', data),
+    registerVerify: (data) => http.post('/auth/register/verify', data),
+    reset: (data) => http.post('/auth/reset', data),
+    resetResend: (data) => http.post('/auth/reset/resend', data),
+    resetConfirm: (data) => http.post('/auth/reset/confirm', data)
+  },
+  comments: {
+    create: (data) => http.post('/comments', data),
+    read: (id) => http.get(`/comments/${id}`),
+    modify: (id, data) => http.patch(`/comments/${id}`, data),
+    delete: (id) => http.delete(`/comments/${id}`),
+    readComments: (id) => http.get(`/comments/${id}/comments`)
+  },
+  cuisines: {
+    readAll: () => http.get('/cuisines')
+  },
+  categories: {
+    readAll: () => http.get('/categories')
+  },
+  recipes: {
+    readAll: (params) => http.get('/recipes', { params: params }),
+    create: (data) => http.post('/recipes', data),
+    search: (params) => http.get('/recipes/search', { params: params }),
+    read: (id) => http.get(`/recipes/${id}`),
+    modify: (id, data) => http.patch(`/recipes/${id}`, data),
+    delete: (id) => http.delete(`/recipes/${id}`),
+    like: (id) => http.post(`/recipes/${id}`),
+    favourite: (id) => http.post(`/recipes/${id}`),
+    readComments: (id, params) => http.get(`/recipes/${id}/comments`, { params: params })
+  },
+  uploads: {
+    create: (data) => http.post('/uploads', data),
+    read: (id) => http.get(`/uploads/${id}`),
+    delete: (id) => http.delete(`/uploads/${id}`)
+  },
+  users: {
+    current: () => http.get('/users/current'),
+    search: (params) => http.get('/users/search', { params: params }),
+    read: (id) => http.get(`/users/${id}`),
+    modify: (id, data) => http.patch(`/users/${id}`, data),
+    follow: (id) => http.post(`/users/${id}/follow`),
+    readRecipes: (id, params) => http.get(`/users/${id}/recipes`, { params: params }),
+    readFavourites: (id, params) => http.get(`/users/${id}/favourites`, { params: params })
+  }
+}
