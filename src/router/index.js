52948e4db3fe95334afe0bcbd7b83eaf6b446814
Piotr Pleban <peper.pleban@gmail.com>
2018-09-30 09:36:34 +0200

    Moved route definitions to separate file.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import routes from '@/router/routes'
 import store from '@/store'
 
 Vue.use(VueRouter)
@@ -7,62 +8,7 @@ Vue.use(VueRouter)
 const router = new VueRouter({
   linkActiveClass: 'is-active',
   mode: 'history',
-  routes: [
-    {
-      path: '/',
-      component: () => import('@/pages/home-page'),
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/favorite',
-      component: () => import('@/pages/favorite-page'),
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/cookbook',
-      component: () => import('@/pages/cookbook-page'),
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/user/:id',
-      component: () => import('@/pages/user-page'),
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/new-recipe',
-      component: () => import('@/pages/new-recipe-page'),
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/recipe/:id',
-      component: () => import('@/pages/recipe-page'),
-      meta: { requiresAuth: true }
-    },
-    {
-      path: '/login',
-      component: () => import('@/pages/login-page')
-    },
-    {
-      path: '/register',
-      component: () => import('@/pages/register-page')
-    },
-    {
-      path: '/reset',
-      component: () => import('@/pages/reset-page')
-    },
-    {
-      path: '/confirm',
-      component: () => import('@/pages/confirm-page')
-    },
-    {
-      path: '/verify',
-      component: () => import('@/pages/verify-page')
-    },
-    {
-      path: '*',
-      redirect: '/'
-    }
-  ]
+  routes
 })
 
 router.beforeEach((to, from, next) => {
---
+++ src/router/routes.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    path: '/',
+    component: () => import('@/pages/home-page'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/favorite',
+    component: () => import('@/pages/favorite-page'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/cookbook',
+    component: () => import('@/pages/cookbook-page'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/user/:id',
+    component: () => import('@/pages/user-page'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/new-recipe',
+    component: () => import('@/pages/new-recipe-page'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/recipe/:id',
+    component: () => import('@/pages/recipe-page'),
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/login',
+    component: () => import('@/pages/login-page')
+  },
+  {
+    path: '/register',
+    component: () => import('@/pages/register-page')
+  },
+  {
+    path: '/reset',
+    component: () => import('@/pages/reset-page')
+  },
+  {
+    path: '/confirm',
+    component: () => import('@/pages/confirm-page')
+  },
+  {
+    path: '/verify',
+    component: () => import('@/pages/verify-page')
+  },
+  {
+    path: '*',
+    redirect: '/'
+  }
+]
