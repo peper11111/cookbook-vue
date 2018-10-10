007eb1122b88019265ff76776ed45bbe05180505
Piotr Pleban <peper.pleban@gmail.com>
2018-10-10 15:19:01 +0200

    Moved user-info to separate component.

--- src/components/recipe/recipe-details.vue
+++ src/components/recipe/recipe-details.vue
@@ -18,59 +18,11 @@
         :model="model"
         class="c-recipe-details__content"
       ></recipe-content>
-      <div class="c-recipe-details__info">
-        <label class="c-recipe-details__item">
-          <span class="c-recipe-details__label">
-            {{ $t('recipe.cuisine-type') }}
-          </span>
-          <form-select
-            v-model="model.cuisineId"
-            :disabled="displayMode || previewMode"
-            :options="cuisines"
-            class="c-recipe-details__value"
-          ></form-select>
-        </label>
-        <label class="c-recipe-details__item">
-          <span class="c-recipe-details__label">
-            {{ $t('recipe.category-type') }}
-          </span>
-          <form-select
-            v-model="model.categoryId"
-            :disabled="displayMode || previewMode"
-            :options="categories"
-            class="c-recipe-details__value"
-          ></form-select>
-        </label>
-        <label class="c-recipe-details__item">
-          <span class="c-recipe-details__label">
-            {{ $t('recipe.difficulty') }}
-          </span>
-          <rating-bar
-            v-model="model.difficulty"
-            :disabled="displayMode || previewMode"
-            class="c-recipe-details__value"
-          ></rating-bar>
-        </label>
-        <label class="c-recipe-details__item">
-          <span class="c-recipe-details__label">
-            {{ $t('recipe.plates') }}
-          </span>
-          <form-input
-            v-model="model.plates"
-            :disabled="displayMode || previewMode"
-            class="c-recipe-details__value"
-          ></form-input>
-        </label>
-        <label class="c-recipe-details__item">
-          <span class="c-recipe-details__label">
-            {{ $t('recipe.preparation-time') }}
-          </span>
-          <time-input
-            v-model="model.preparationTime"
-            :disabled="displayMode || previewMode"
-          ></time-input>
-        </label>
-      </div>
+      <recipe-info
+        :mode="localMode"
+        :model="model"
+        class="c-recipe-details__info"
+      ></recipe-info>
     </div>
   </div>
 </div>
@@ -84,12 +36,8 @@ export default {
   name: 'RecipeDetails',
   components: {
     DetailActions: () => import('@/components/detail-actions'),
-    FormInput: () => import('@/components/form/form-input'),
-    FormSelect: () => import('@/components/form/form-select'),
-    FormTextarea: () => import('@/components/form/form-textarea'),
     ImagePicker: () => import('@/components/form/image-picker'),
-    RatingBar: () => import('@/components/form/rating-bar'),
-    TimeInput: () => import('@/components/form/time-input'),
+    RecipeInfo: () => import('@/components/recipe/recipe-info'),
     RecipeContent: () => import('@/components/recipe/recipe-content')
   },
   mixins: [ detail ],
@@ -109,22 +57,6 @@ export default {
     }
   },
   computed: {
-    cuisines () {
-      return this.$store.state.cuisines.map((cuisine) => {
-        return {
-          value: cuisine.id,
-          label: cuisine.name
-        }
-      })
-    },
-    categories () {
-      return this.$store.state.categories.map((category) => {
-        return {
-          value: category.id,
-          label: category.name
-        }
-      })
-    },
     recipe () {
       return this.$store.state.recipe
     }
@@ -165,30 +97,12 @@ export default {
   }
 
   &__content {
-    display: flex;
-    flex-direction: column;
     flex-grow: 1;
     margin-right: 32px;
   }
 
   &__info {
     width: 300px;
-  }
-
-  &__item {
-    display: flex;
-    align-items: center;
-    margin-bottom: 16px;
-  }
-
-  &__label {
-    text-align: left;
-    white-space: nowrap;
-    width: 140px;
-  }
-
-  &__value {
-    width: 160px;
   }
 }
 </style>
---
+++ src/components/recipe/recipe-info.vue
@@ -0,0 +1,121 @@
+<template>
+<div class="c-recipe-info">
+  <div class="c-recipe-info__item">
+    <span class="c-recipe-info__label">
+      {{ $t('recipe.cuisine-type') }}
+    </span>
+    <form-select
+      v-model="model.cuisineId"
+      :disabled="displayMode || previewMode"
+      :options="cuisines"
+      class="c-recipe-info__value"
+    ></form-select>
+  </div>
+  <div class="c-recipe-info__item">
+    <span class="c-recipe-info__label">
+      {{ $t('recipe.category-type') }}
+    </span>
+    <form-select
+      v-model="model.categoryId"
+      :disabled="displayMode || previewMode"
+      :options="categories"
+      class="c-recipe-info__value"
+    ></form-select>
+  </div>
+  <div class="c-recipe-info__item">
+    <span class="c-recipe-info__label">
+      {{ $t('recipe.difficulty') }}
+    </span>
+    <rating-bar
+      v-model="model.difficulty"
+      :disabled="displayMode || previewMode"
+      class="c-recipe-info__value"
+    ></rating-bar>
+  </div>
+  <div class="c-recipe-info__item">
+    <span class="c-recipe-info__label">
+      {{ $t('recipe.plates') }}
+    </span>
+    <form-input
+      v-model="model.plates"
+      :disabled="displayMode || previewMode"
+      class="c-recipe-info__value"
+    ></form-input>
+  </div>
+  <div class="c-recipe-info__item">
+    <span class="c-recipe-info__label">
+      {{ $t('recipe.preparation-time') }}
+    </span>
+    <time-input
+      v-model="model.preparationTime"
+      :disabled="displayMode || previewMode"
+    ></time-input>
+  </div>
+</div>
+</template>
+
+<script>
+import { DISPLAY, PREVIEW } from '@/mixins/detail/modes'
+
+export default {
+  name: 'RecipeInfo',
+  components: {
+    FormInput: () => import('@/components/form/form-input'),
+    FormSelect: () => import('@/components/form/form-select'),
+    FormTextarea: () => import('@/components/form/form-textarea'),
+    RatingBar: () => import('@/components/form/rating-bar'),
+    TimeInput: () => import('@/components/form/time-input')
+  },
+  props: {
+    mode: String,
+    model: Object
+  },
+  computed: {
+    displayMode () {
+      return this.mode === DISPLAY
+    },
+    previewMode () {
+      return this.mode === PREVIEW
+    },
+    cuisines () {
+      return this.$store.state.cuisines.map((cuisine) => {
+        return {
+          value: cuisine.id,
+          label: cuisine.name
+        }
+      })
+    },
+    categories () {
+      return this.$store.state.categories.map((category) => {
+        return {
+          value: category.id,
+          label: category.name
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.c-recipe-info {
+  display: flex;
+  flex-direction: column;
+
+  &__item {
+    display: flex;
+    align-items: center;
+    margin-bottom: 16px;
+  }
+
+  &__label {
+    text-align: left;
+    white-space: nowrap;
+    width: 140px;
+  }
+
+  &__value {
+    width: 160px;
+  }
+}
+</style>
