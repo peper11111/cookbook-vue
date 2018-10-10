a39be3a5499f50a11c6f45d1369942d23f2e07d9
Piotr Pleban <peper.pleban@gmail.com>
2018-10-10 12:20:52 +0200

    Simplified textarea component.

--- src/components/form/form-textarea.vue
+++ src/components/form/form-textarea.vue
@@ -1,18 +1,16 @@
 <template>
-<div>
-  <textarea
-    v-if="!disabled"
-    :placeholder="placeholder"
-    :value="localValue"
-    :rows="rows"
-    :maxlength="maxlength"
-    @input="setLocalValue($event.target.value)"
-    class="o-form__textarea"
-  ></textarea>
-  <p v-if="disabled">
-    {{ localValue }}
-  </p>
+<div v-if="disabled">
+  {{ value }}
 </div>
+<textarea
+  v-else
+  :placeholder="placeholder"
+  :value="value"
+  :rows="rows"
+  :maxlength="maxlength"
+  @input="onInput"
+  class="o-form__textarea"
+></textarea>
 </template>
 
 <script>
@@ -22,23 +20,18 @@ export default {
     disabled: Boolean,
     placeholder: String,
     value: String,
-    rows: Number,
-    maxlength: Number
-  },
-  data () {
-    return {
-      localValue: this.value
-    }
-  },
-  watch: {
-    value (val) {
-      this.localValue = val
+    rows: {
+      default: 3,
+      type: Number
+    },
+    maxlength: {
+      default: 255,
+      type: Number
     }
   },
   methods: {
-    setLocalValue (val) {
-      this.localValue = val
-      this.$emit('input', this.localValue)
+    onInput (event) {
+      this.$emit('input', event.target.value)
     }
   }
 }
