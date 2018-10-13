0ecabfa873c7f92d79d40ce18f22a14d26f336c2
Piotr Pleban <peper.pleban@gmail.com>
2018-10-13 23:57:52 +0200

    Created simple comment-input component.

---
+++ src/components/form/comment-input.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="c-comment-input">
+  <img
+    :src="avatarSrc"
+    class="c-comment-input__image"
+  />
+  <div class="c-comment-input__wrapper">
+    <input
+      v-model="comment"
+      :placeholder="$t('comment.placeholder')"
+      class="o-form__input o-form__input--full"
+    />
+    <div class="c-comment-input__buttons">
+      <button
+        @click="comment = ''"
+        class="o-button o-button__primary"
+      >
+        {{ $t('global.cancel') }}
+      </button>
+      <button class="o-button o-button__accent">
+        {{ $t('global.comment') }}
+      </button>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import config from '@/config'
+
+export default {
+  name: 'CommentInput',
+  data () {
+    return {
+      comment: ''
+    }
+  },
+  computed: {
+    authUser () {
+      return this.$store.state.auth.user
+    },
+    avatarSrc () {
+      return this.$helpers.thumbnailSrc(this.authUser.avatarId) || config.blankAvatar
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.c-comment-input {
+  display: flex;
+
+  &__image {
+    width: 50px;
+    height: 50px;
+    object-fit: cover;
+  }
+
+  &__wrapper {
+    flex-grow: 1;
+    margin-left: 16px;
+  }
+
+  &__buttons {
+    display: flex;
+    justify-content: flex-end;
+  }
+}
+</style>
--- src/components/recipe/recipe-comments.vue
+++ src/components/recipe/recipe-comments.vue
@@ -3,6 +3,7 @@
   <h1 class="c-recipe-comments__title">
     {{ $t('recipe.comments') }}
   </h1>
+  <comment-input class="c-recipe-comments__input"></comment-input>
   <comment-list
     :recipeId="recipe.id"
     type="recipe-comments"
@@ -14,6 +15,7 @@
 export default {
   name: 'RecipeComments',
   components: {
+    CommentInput: () => import('@/components/form/comment-input'),
     CommentList: () => import('@/components/list/comment-list')
   },
   computed: {
@@ -30,6 +32,10 @@ export default {
 
   &__title {
     font-size: 24px;
+    margin-bottom: 16px;
+  }
+
+  &__input {
     margin-bottom: 16px;
   }
 }
