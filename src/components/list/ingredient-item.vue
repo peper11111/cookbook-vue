e39c04b243c1b230b2680417f2ab4c06ea805a4b
Piotr Pleban <peper.pleban@gmail.com>
2018-10-14 14:41:05 +0200

    Refactored ingredient-item template.

--- src/components/list/ingredient-item.vue
+++ src/components/list/ingredient-item.vue
@@ -1,25 +1,29 @@
 <template>
-<div class="c-ingredient-item">
+<div
+  :class="{ 'c-ingredient-item--center': !previewMode }"
+  class="c-ingredient-item"
+>
   <i
+    v-if="previewMode"
     :class="{ 'is-checked': checked }"
     @click="checked = !checked"
     class="material-icons c-ingredient-item__icon"
   >
     {{ checked ? 'check_box' : 'check_box_outline_blank' }}
   </i>
-  <span
-    :class="{ 'is-checked': checked }"
-    class="c-ingredient-item__label"
-  >
-    {{ ingredient }}
-  </span>
   <i
-    v-if="!previewMode"
+    v-else
     @click="$emit('delete')"
     class="material-icons c-ingredient-item__remove"
   >
     remove
   </i>
+  <form-input
+    v-model="ingredient"
+    :class="{ 'is-checked': checked }"
+    :disabled="previewMode"
+    class="c-ingredient-item__label"
+  ></form-input>
 </div>
 </template>
 
@@ -28,6 +32,9 @@ import modeContext from '@/mixins/detail/mode-context'
 
 export default {
   name: 'IngredientItem',
+  components: {
+    FormInput: () => import('@/components/form/form-input')
+  },
   mixins: [ modeContext ],
   props: {
     ingredient: String
@@ -48,6 +55,10 @@ export default {
   margin-bottom: 8px;
   line-height: 24px;
 
+  &--center {
+    align-items: center;
+  }
+
   &__icon {
     cursor: pointer;
 
@@ -58,6 +69,7 @@ export default {
 
   &__label {
     margin: 0 8px;
+    flex-grow: 1;
 
     &.is-checked {
       color: $color-text-secondary;
