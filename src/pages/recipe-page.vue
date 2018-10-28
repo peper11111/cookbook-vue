a64a7e420bddfec04ae0166d29871d6793ac0d3c
Piotr Pleban <peper.pleban@gmail.com>
2018-10-28 23:16:04 +0100

    Moved recipe-comments content to recipe-page.

--- src/pages/recipe-page.vue
+++ src/pages/recipe-page.vue
@@ -7,7 +7,18 @@
     <recipe-details></recipe-details>
     <div class="o-page__separator"></div>
     <div class="c-recipe-page__wrapper">
-      <recipe-comments></recipe-comments>
+      <h1 class="o-typography__header">
+        {{ $t('recipe.comments') }}
+      </h1>
+      <comment-input
+        :recipeId="recipe.id"
+        @refresh="$emit('refresh')"
+        class="c-recipe-page__input"
+      ></comment-input>
+      <comment-list
+        :recipeId="recipe.id"
+        type="recipe-comments"
+      ></comment-list>
     </div>
   </div>
 </div>
@@ -21,8 +32,14 @@ export default {
   name: 'RecipePage',
   mixins: [ requester ],
   components: {
-    RecipeComments: () => import('@/components/recipe/recipe-comments'),
+    CommentInput: () => import('@/components/form/comment-input'),
+    CommentList: () => import('@/components/list/comment-list'),
     RecipeDetails: () => import('@/components/recipe/recipe-details')
+  },
+  computed: {
+    recipe () {
+      return this.$store.state.recipe
+    }
   },
   created () {
     this.wrap(this.fetchRecipe())
@@ -41,6 +58,10 @@ export default {
 .c-recipe-page {
   &__wrapper {
     padding: 0 32px;
+  }
+
+  &__input {
+    margin-bottom: 16px;
   }
 }
 </style>
