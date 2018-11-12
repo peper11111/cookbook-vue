22f26752c9be0b81204d29fc56269dc86fbda5f2
Piotr Pleban <peper.pleban@gmail.com>
2018-11-12 17:30:24 +0100

    Removed unnecessary container.

--- src/components/list/recipe-buttons.vue
+++ src/components/list/recipe-buttons.vue
@@ -1,21 +1,19 @@
 <template>
 <div class="c-recipe-buttons">
-  <div class="c-recipe-buttons__icons">
-    <i
-      :class="{ 'is-active': isActiveLayout('list') }"
-      @click="setActiveLayout('list')"
-      class="material-icons"
-    >
-      view_list
-    </i>
-    <i
-      :class="{ 'is-active': isActiveLayout('grid') }"
-      @click="setActiveLayout('grid')"
-      class="material-icons"
-    >
-      view_module
-    </i>
-  </div>
+  <i
+    :class="{ 'is-active': isActiveLayout('list') }"
+    @click="setActiveLayout('list')"
+    class="material-icons"
+  >
+    view_list
+  </i>
+  <i
+    :class="{ 'is-active': isActiveLayout('grid') }"
+    @click="setActiveLayout('grid')"
+    class="material-icons"
+  >
+    view_module
+  </i>
 </div>
 </template>
 
@@ -47,23 +45,18 @@ export default {
   align-items: center;
   justify-content: flex-end;
 
-  &__icons {
-    display: flex;
-    align-items: center;
+  .material-icons {
+    color: $color-gray-300;
+    margin-left: 8px;
+    cursor: pointer;
+    user-select: none;
 
-    .material-icons {
-      color: $color-gray-300;
-      margin-left: 8px;
-      cursor: pointer;
-      user-select: none;
+    &:first-child {
+      margin-left: 0;
+    }
 
-      &:first-child {
-        margin-left: 0;
-      }
-
-      &.is-active {
-        color: $color-black;
-      }
+    &.is-active {
+      color: $color-black;
     }
   }
 }
